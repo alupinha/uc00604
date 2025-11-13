37f84e97f9a47aa1ab4b6de9fdc89ba6cefd7354
Paula <paularoessle@gmail.com>
2025-11-13 22:11:45 +0000

    Correções criação dos componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./componentes/header/header";
+import { Main } from "./componentes/main/main";
+import { Footer } from "./componentes/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Main, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
