d9b670e85ee807f92883caa52fb4440ddf810a62
Paula <paularoessle@gmail.com>
2025-11-27 20:07:53 +0000

    Correção rota 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   /**Primeira rota */
   {path:'', redirectTo: "welcome", pathMatch: 'full'},
+  {path:"welcome", loadComponent: () => import ('./pages/welcome/welcome').then(c => c.Welcome) },
+
 
   /**Ultima rota */
   {path:"**", loadComponent: () => import('./pages/welcome/welcome').then(c => c.Welcome)},
