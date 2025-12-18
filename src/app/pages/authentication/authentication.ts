3ad65d5312b021ec42c66b4c645d39a6deb54395
Paula <paularoessle@gmail.com>
2025-12-18 21:24:36 +0000

    authentication

--- src/app/pages/authentication/authentication.ts
+++ src/app/pages/authentication/authentication.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-authentication',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './authentication.html',
   styleUrl: './authentication.css',
 })
 export class Authentication {
+
+   protected signinForms!: UntypedFormGroup;
+
+   constructor(private fb: UntypedFormBuilder) {
+       this.signinForms = this.fb.nonNullable.group({
+         email: [''],
+         password:['']
+       });
+
+
+     console.log('meu form', this.signinForms.value)
+
+   }
 
 }
